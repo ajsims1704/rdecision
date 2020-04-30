c6bd441635aa8b9559726255bfa5fb4dfb474572
ajsims1704 <64521473+ajsims1704@users.noreply.github.com>
2020-04-30 23:59:19 +0100

    Tegaderm model updates

--- R/BetaModelVariable.R
+++ R/BetaModelVariable.R
@@ -1,23 +1,21 @@
 #' @title 
-#' GammaModelVariable
+#' BetaModelVariable
 #' 
 #' @description
-#' An R6 class for a model variable with Gamma function uncertainty
+#' An R6 class for a model variable with Beta function uncertainty
 #' 
 #' @details 
 #' A model variable for which the uncertainty in the point estimate can
-#' be modelled with a Gamma distribution. The hyperparameters of the
-#' distribution are the shape (`alpha`) and the scale (`beta`) of
-#' the uncertainty distribution. Note that this variable naming 
-#' convention follos Briggs et al; `beta` is more usually used to
-#' describe the rate parameter (reciprocal of scale.)
+#' be modelled with a Beta distribution. The hyperparameters of the
+#' distribution are the shape (`alpha`) and the shape (`beta`) of
+#' the uncertainty distribution. 
 #'
 #' @docType class
 #' @author Andrew J. Sims \email{andrew.sims5@nhs.net}
 #' @export
 #' 
-GammaModelVariable <- R6::R6Class(
-  classname = "GammaModelVariable",
+BetaModelVariable <- R6::R6Class(
+  classname = "BetaModelVariable",
   inherit = ModelVariable,
   private = list(
     alpha = 'numeric',
@@ -26,17 +24,17 @@ GammaModelVariable <- R6::R6Class(
   public = list(
     
     #' @description 
-    #' Create an object of class GammaModelVariable.
+    #' Create an object of class BetaModelVariable.
     #' @param description A character string describing the variable.
     #' @param units Units of the variable, as character string.
-    #' @param alpha shape parameter of the Gamma distribution.
-    #' @param beta scale parameter of the Gamma distribution.
-    #' @return An object of class GammaModelVariable. 
+    #' @param alpha parameter of the Beta distribution.
+    #' @param beta parameter of the Beta distribution.
+    #' @return An object of class BetaModelVariable. 
     initialize = function(description, units, alpha, beta) {
       super$initialize(description, units)
       private$alpha <- alpha
       private$beta <- beta
-      private$val <- private$alpha * private$beta
+      private$val <- self$getMean()
     },
     
     #' @description
@@ -46,14 +44,14 @@ GammaModelVariable <- R6::R6Class(
     #' @param expected Logical; if TRUE sets the value of the model variable
     #'        returned at subsequent calls to `value()` to be equal to the 
     #'        expectation of the variable. Default is FALSE.
-    #' @return Updated GammaModelVariable object.
+    #' @return Updated BetaModelVariable object.
     sample = function(expected=F) {
       private$val <- NA
       if (expected) {
         private$val <- self$getMean()
       }
       else {
-        private$val <- rgamma(1, shape=private$alpha, scale=private$beta)
+        private$val <- rbeta(1, shape1=private$alpha, shape2=private$beta)
       }
       return(invisible(self))
     },
@@ -62,7 +60,7 @@ GammaModelVariable <- R6::R6Class(
     #' Accessor function for the name of the uncertainty distribution.
     #' @return Distribution name as character string.
     getDistribution = function() {
-      rv <- paste('Ga(', round(private$alpha,3), ',', round(private$beta,3), ')', sep='')
+      rv <- paste('Be(', private$alpha, ',', private$beta, ')', sep='')
       return(rv)
     },
     
@@ -70,16 +68,19 @@ GammaModelVariable <- R6::R6Class(
     #' Return the expected value of the distribution. 
     #' @return Expected value as a numeric value.
     getMean = function() {
-      return(private$alpha*private$beta)
+      return(private$alpha/(private$alpha+private$beta))
     },
     
     #' @description 
     #' Return the standard deviation of the distribution. 
     #' @return Standard deviation as a numeric value
     getSD = function() {
-      return(sqrt(private$alpha)*private$beta)
+      a <- private$alpha
+      b <- private$beta
+      v <- (a*b) / ( (a+b)^2 * (a+b+1) )
+      return(sqrt(v))
     },
-
+    
     #' @description
     #' Return the quantiles of the Gamma uncertainty distribution.
     #' @param probs Vector of probabilities, in range [0,1].    
@@ -87,12 +88,12 @@ GammaModelVariable <- R6::R6Class(
     getQuantile = function(probs) {
       sapply(probs, FUN=function(x) {
         if (!is.numeric(probs)) {
-          stop("GammaModelVariable$getQuantile: argument must be a numeric vector")
+          stop("BetaModelVariable$getQuantile: argument must be a numeric vector")
         }
       })
-      q <- qgamma(probs, shape=private$alpha, scale=private$beta)
+      q <- qbeta(probs, shape1=private$alpha, shape2=private$beta)
       return(q)
     }
-
+    
   )
 )
