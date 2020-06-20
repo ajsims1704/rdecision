f99a8af755e7aa5709e4eb2e01cd14691d6c15bd
ajsims1704 <64521473+ajsims1704@users.noreply.github.com>
2020-06-20 17:01:47 +0100

    Replaced DecisionTree

--- R/DecisionTree.R
+++ R/DecisionTree.R
@@ -6,15 +6,28 @@
 #' 
 #' @details 
 #' A class to represent a decision tree. An object contains a tree of
-#' decision nodes, chance nodes and states, connected by edges, and
-#' meeting the criteria for a decision tree.
-#' 
-#' @note
-#' In graph theory a decision tree is a k-ary directed rooted tree, or 
-#' 'arborescence'. This is considered a form of directed graph (digraph) by
-#' several authors. In `rdecision` the root is a DecisionNode and in decision 
-#' trees used in health
-#' economics there is an implied directionality, from root to leaf nodes.
+#' decision nodes, chance nodes and leaf nodes, connected by edges
+#' (either actions or reactions) and which satisfies the following
+#' conditions:
+#' \enumerate{
+#' \item{Each node must inherit from one of \code{DecisionNode},
+#' \code{ChanceNode} or \code{LeafNode}. Formally the set of vertices
+#' must be a disjoint union of sets of decision nodes, chance nodes
+#' and leaf nodes.}
+#' \item{Each edge must inherit from either \code{Action} or
+#' \code{Reaction}.}
+#' \item{Nodes and edges must form a tree with a single root and
+#' there must be a unique path from the root to each node.
+#' In graph theory, the directed graph formed by the nodes and edges must
+#' be an \dfn{arborescence}.}
+#' \item{All and only leaf nodes must have no children.}
+#' \item{All and only edges that have source endpoints joined to 
+#' decision nodes must inherit from \code{Action}.}
+#' \item{All and only edges that have source endpoints joined to 
+#' chance nodes must inherit from \code{Reaction}.}
+#' \item{The sum of probabilities of each set of reaction edges 
+#' with a common source endpoint must be 1.}
+#' }
 #' 
 #' @docType class
 #' @author Andrew J. Sims \email{andrew.sims@@newcastle.ac.uk}
@@ -22,130 +35,40 @@
 #' 
 DecisionTree <- R6::R6Class(
   classname = "DecisionTree",
+  inherit = Arborescence,
   private = list(
-    dn = NULL
   ),
   public = list(
     
     #' @description 
-    #' Create a new decision tree. The tree must start with a decision node
-    #' which is the root of a tree of chance nodes and state nodes
-    #' connected by edges.
-    #' @param dn An object inheriting from class DecisionNode.
+    #' Create a new decision tree. The tree must consist of a set of
+    #' nodes and a set of edges which satisfy the conditions given
+    #' in the details section of this class.
+    #' @param V A list of nodes.
+    #' @param E A list of edges.
     #' @return A DecisionTree object
-    initialize = function(dn) {
-      # check and set dn
-      if (!inherits(dn, what="DecisionNode")) {
-        rlang::abort("Argument 'dn' must of class 'DecisionNode'",
-                     class="non-DecisionNode_root")
-      }
-      private$dn <- dn
-      return(invisible(self))
-    },
-    
-    #' @description 
-    #' Update the tree by sampling model variables.
-    #' @param expected if TRUE set model variables to their expectation; 
-    #' otherwise sample from their uncertainty distributions.
-    #' @returns Updated DecisionNode object.
-    update = function(expected=TRUE) {
-      # sample model variables of the root node and all its descendants
-      descendants <- private$dn$descendantNodes()
-      lapply(descendants, FUN=function(n) {
-        n$sample_modvars(expected)
-      })
-      # return updated DecisionNode
-      return(invisible(self))
-    }, 
-    
-    #' @description 
-    #' Evaluate a decision. Starting with this decision node, the function
-    #' works though all possible paths and computes the probability,
-    #' cost and utility of each. 
-    #' @param expected If TRUE, evaluate each model variable as its mean value,
-    #'        otherwise sample each one from their uncertainty distrbution.
-    #' @param uncorrelate If TRUE, resample and update the tree between
-    #' the evaluation of each choice. This causes any model variables that
-    #' are common to more than one choice to be resampled between choices,
-    #' and removes correlation due to shared model variables. Other forms
-    #' of correlation may not be removed.
-    #' @return A data frame with one row per path and columns organized as
-    #' follows:
-    #' \describe{
-    #' \item{Choice}{The choice with which the path is associated.}
-    #' \item{Pathway}{The leaf node on which the pathway ends; normally the 
-    #' clinical outcome.}
-    #' \item{Probability}{The probability of traversing the pathway. The total
-    #' probability of each choice should sum to unity; i.e. the sum of the 
-    #' Probability column should equal the number of branches leaving the 
-    #' decision node.}
-    #' \item{Cost}{The cost of traversing the pathway.}
-    #' \item{ExpectedCost}{Cost \eqn{*} probability of traversing the pathway.}
-    #' \item{Utility}{The utility associated with the outcome.}
-    #' \item{ExpectedUtility}{Utility \eqn{*} probability of traversing the pathway.}
-    #' }
-    evaluatePathways = function(expected=TRUE, uncorrelate=FALSE) {
-      # if no requirement to uncorrelate, resample the tree once
-      if (!uncorrelate) {
-        self$update(expected)
-      }
-      # choice by choice
-      choices <- private$dn$get_choices()
-      choicerows <- lapply(choices, FUN=function(choice) {
-        if (uncorrelate) {
-          self$update(expected)
+    initialize = function(V,E) {
+      # check the types of each node
+      sapply(V, function(v) {
+        if (inherits(v,what="DecisionNode")) {
+          
+        } else if (inherits(v, what="ChanceNode")) {
+          
+        } else if (inherits(v, what="LeafNode")) {
+          
+        } else {
+          rlang::abort(
+            "Each node must be a 'DecisionNode', 'ChanceNode' or 'LeafNode'.",
+            class="incorrect_node_type")
         }
-        paths <- private$dn$getPathways(choice)
-        RCH <- do.call('rbind', lapply(paths, FUN=function(x){x$tabulate()}))
-        return(RCH)
       })
-      RES <- do.call('rbind', choicerows)
-      # add expected cost and utility     
-      RES$ExpectedCost <- RES$Probability*RES$Cost
-      RES$ExpectedUtility <- RES$Probability*RES$Utility
-      return(RES)
-    },
-    
-    #' @description 
-    #' Evaluate each choice. Starting with this decision node, the function
-    #' works though all possible paths and computes the probability,
-    #' cost and utility of each, then aggregates by choice. 
-    #' @param expected If TRUE, evaluate each model variable as its mean value,
-    #'        otherwise sample each one from their uncertainty distrbution.
-    #' @param uncorrelate If TRUE, resample and update the tree between
-    #' the evaluation of each choice. This causes any model variables that
-    #' are common to more than one choice to be resampled between choices,
-    #' and removes correlation due to shared model variables.
-    #' @param N Number of replicates. Intended for use with PSA (expected=F);
-    #' use with expected=T will be repetitive and uninformative. 
-    #' @return A data frame with one row per choice per run and columns
-    #' organized as follows:
-    #' \describe{
-    #' \item{Run}{The run number}
-    #' \item{Choice}{The choice.}
-    #' \item{Cost}{Aggregate cost of the choice.}
-    #' \item{Utility}{Aggregate utility of the choice.}
-    #' }
-    evaluateChoices = function(expected=TRUE, uncorrelate=FALSE, N=1) {
-      DF <- do.call('rbind', lapply(1:N, FUN=function(n){
-        # evaluate pathways
-        RES <- self$evaluatePathways(expected, uncorrelate)
-        RES$Choice <- as.character(RES$Choice)
-        # aggregate them by choice
-        SUM <- aggregate(
-          RES[,c('ExpectedCost', 'ExpectedUtility')],
-          by = list(RES$Choice),
-          FUN = sum
-        )
-        names(SUM) <- c('Choice', 'Cost', 'Utility')
-        SUM <- cbind(Run=rep(n, times=nrow(SUM)), SUM)
-        # return the aggregates
-        return(SUM)
-      }))
-      return(DF)
+      # initialize the base class(es)
+      super$initialize(V,E)
+      # return a new DecisionTree object
+      return(invisible(self))
     }
+    
   )
 )
 
 
-   
