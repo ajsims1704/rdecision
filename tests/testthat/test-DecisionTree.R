74b0c2a6e66ecd26abc9f0f1923b748225706b8e
ajsims1704 <64521473+ajsims1704@users.noreply.github.com>
2020-06-28 22:20:47 +0100

    Update test-DecisionTree.R

--- tests/testthat/test-DecisionTree.R
+++ tests/testthat/test-DecisionTree.R
@@ -107,7 +107,7 @@ test_that("rdecision replicates Evans et al, Sumatriptan base case", {
 # -----------------------------------------------------------------------------
 # Kaminski et al CEJOR 2018;26:135-139, fig 7 (gas problem)
 # -----------------------------------------------------------------------------
-test_that("rdecision Kaminski et al, fig 7", {
+test_that("rdecision replacates Kaminski et al, fig 7", {
   # nodes
   d1 <- DecisionNode$new("d1")
   d2 <- DecisionNode$new("d2")
@@ -125,13 +125,42 @@ test_that("rdecision Kaminski et al, fig 7", {
   t7 <- LeafNode$new("t7")
   t8 <- LeafNode$new("t8")
   t9 <- LeafNode$new("t9")
+  # probabilities
+  p.sens <- 0.9
+  p.spec <- 0.7
+  p.gas <- 0.7
+  p.nogas <- 1-p.gas
+  p.ptest <- p.sens*p.gas + (1-p.spec)*p.nogas
+  p.ntest <- (1-p.sens)*p.gas + p.spec*p.nogas
+  p.gas.ptest <- p.sens*p.gas / p.ptest
+  p.gas.ntest <- (1-p.sens)*p.gas / p.ntest
   # edges
-  d1t1 <- Action$new(d1,t1,"sell",benefit=800)
-  #
-  d1c1 <- Action$new(d1,c1,"dig",cost=300)
-  c1t2 <- Reaction$new(c1,t2,"gas",benefit=2500)
-  c1t3 <- reaction$new(c1,t3,"no gas")
-  #
-  d1c2 <- Action$new(d1,c2,"test",cost=50)
-  
+  E <- list(
+    Action$new(d1,t1,"sell",benefit=800),
+    Action$new(d1,c1,"dig",cost=300),
+    Reaction$new(c1,t2,p=p.gas,benefit=2500,label="gas"),
+    Reaction$new(c1,t3,p=p.nogas,label="no gas"),
+    Action$new(d1,c2,"test",cost=50),
+    Reaction$new(c2,d2,p=p.ntest,label="negative"),
+    Action$new(d2,t4,"sell",benefit=600),
+    Action$new(d2,c3,"dig",cost=300),
+    Reaction$new(c3,t5,p=p.gas.ntest,benefit=2500,label="gas"),
+    Reaction$new(c3,t6,p=(1-p.gas.ntest),label="no gas"),
+    Reaction$new(c2,d3,p=p.ptest,label="positive"),
+    Action$new(d3,t7,"sell",benefit=1000),
+    Action$new(d3,c4,"dig",cost=300),
+    Reaction$new(c4,t8,p=p.gas.ptest,benefit=2500,label="gas"),
+    Reaction$new(c4,t9,p=(1-p.gas.ptest),label="no gas")
+  )
+  # tree
+  V <- list(d1,d2,d3, c1,c2,c3,c4, t1,t2,t3,t4,t5,t6,t7,t8,t9)
+  expect_silent(DT<-DecisionTree$new(V,E))
+  # evaluate
+  RES <- DT$evaluate_paths()
+  print("DT")
+  print(RES)
+  expect_true(is.data.frame(RES))
+  RES <- DT$evaluate()
+  print("DT")
+  print(RES)
 })
