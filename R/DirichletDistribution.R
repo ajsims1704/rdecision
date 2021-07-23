9db85471a7c76f2d8ea497ee3ca378d2b53ad434
ajsims1704 <64521473+ajsims1704@users.noreply.github.com>
2021-07-23 18:26:02 +0100

    Implelenting DirichletDistribution

--- R/DirichletDistribution.R
+++ R/DirichletDistribution.R
@@ -1,7 +1,6 @@
 #' @title \verb{DirichletDistribution} class
 #' 
-#' @description
-#' An R6 class for a multivariate Dirichlet distribition.
+#' @description An R6 class for a multivariate Dirichlet distribution.
 #' 
 #' @details A multivariate Dirichlet distribution. Modelled as a series of
 #' conditional beta distributions. 
@@ -10,7 +9,7 @@
 #' @author Andrew J. Sims \email{andrew.sims@@newcastle.ac.uk}
 #' @export
 #' 
-DirichletDistribition <- R6::R6Class(
+DirichletDistribution <- R6::R6Class(
   classname = "DirichletDistribution",
   lock_class = TRUE,
   inherit = Distribution,
@@ -21,45 +20,50 @@ DirichletDistribition <- R6::R6Class(
     
     #' @description 
     #' Create an object of class \code{DirichletDistribution}.
-    #' @param k Order (an integer).
-    #' @param alpha Parameters of the distribution; a vector of numeric values
-    #' each > 0.
+    #' @param K Order (integer \eqn{\ge 2}).
+    #' @param alpha Parameters of the distribution; a vector of \code{K} numeric
+    #' values each > 0.
     #' @return An object of class \code{DirichletDistribution}. 
-    initialize = function(k, alpha) {
-      super$initialize("Dir", k)
+    initialize = function(K, alpha) {
+      # create subclass object and check parameter
+      super$initialize("Dir", K)
+      if (K < 2) {
+        rlang::abort("'K' must be greater than 1", class = "K_unsupported")
+      }
       # check alpha parameter
-      if (!is.numeric(alpha)) {
-        rlang::abort(
-          "Argument 'alpha' must be numeric", 
-          class="alpha_not_numeric"
-        )
-      }
-      if (alpha <= 0) {
-        rlang::abort(
-          "Argument 'alpha' must be > 0", 
-          class="alpha_not_supported"
-        )
-      }
+      sapply(alpha, FUN=function(x) {
+        if (is.na(x)) {
+          rlang::abort("All elements of 'alpha' must be defined",
+                       class="alpha_not_defined")
+        }
+        if (!is.numeric(x)) {
+          rlang::abort("Argument 'alpha' must be a numeric vector",
+                       class="alpha_not_numeric")
+        }
+        if (x<=0) {
+          rlang::abort("Elements of 'alpha' must be > 0",
+                       class="alpha_unsupported")
+        }
+      })
       private$alpha <- alpha
-      # check beta parameter
-      if (!is.numeric(beta)) {
+      # check that the order and number of parameters are equal
+      if (K != length(alpha)) {
         rlang::abort(
-          "Argument 'beta must be numeric", 
-          class="beta_not_numeric"
+          "'alpha' must have K elements", 
+          class="inconsistent_K_alpha"
         )
       }
-      if (beta <= 0) {
-        rlang::abort(
-          "Argument 'beta' must be > 0", 
-          class="beta_not_supported"
-        )
-      }
-      private$beta <- beta
-      # ensure first call to get() is valid
-      self$set("expected")
-      # return BetaModVar
+      # return Dirichlet distribution object
       return(invisible(self))
+    },
+    
+    #' @description Accessor function for the name of the distribution.
+    #' @return Distribution name as character string.
+    distribution = function() {
+      rv <- paste('Dir(', paste(private$alpha, collapse=','), ')', sep='')
+      return(rv)
     }
+    
   )
 )
     
---
+++ tests/testthat/test-DirichletDistribution.R
@@ -0,0 +1,93 @@
+test_that("illegal initializations are rejected", {
+  K <- as.integer(2)
+  alpha <- c(2,6)
+  expect_silent(DirichletDistribution$new(K, alpha))
+  expect_error(DirichletDistribution$new("9",alpha), class="invalid_order")
+  expect_error(DirichletDistribution$new(K,"0.5"), class="alpha_not_numeric")
+  expect_error(DirichletDistribution$new(K,c(2,NA)), class="alpha_not_defined")
+  expect_error(DirichletDistribution$new(K,c(2,0)), class="alpha_unsupported")
+  expect_error(
+    DirichletDistribution$new(K,c(2,1,1)), class="inconsistent_K_alpha"
+  )
+  expect_error(
+    DirichletDistribution$new(as.integer(1), c(2)),
+    class = "K_unsupported"
+  )
+})
+
+test_that("the correct distribution name is created", {
+  K <- as.integer(2)
+  alpha <- c(2,6)
+  D <- DirichletDistribution$new(K, alpha)
+  expect_equal(D$distribution(), "Dir(2,6)")
+})
+ 
+# test_that("mean, mode, sd and quantiles are returned correctly", {
+#   alpha <- 2
+#   beta <- 5
+#   b <- BetaDistribution$new(alpha, beta)
+#   m <- alpha/(alpha+beta)
+#   v <- (alpha*beta)/((alpha+beta+1)*(alpha+beta)^2)
+#   o <- (alpha-1)/(alpha+beta-2)
+#   expect_intol(b$mean(), m, 0.01)
+#   expect_intol(b$SD(), sqrt(v), 0.01)
+#   expect_intol(b$mode(), o, 0.01)
+#   probs <- c(0.025, 0.975)
+#   q <- b$quantile(probs)
+#   expect_intol(q[1], 0.043, 0.01)
+#   expect_intol(q[2], 0.641, 0.01)
+# })
+# 
+# test_that("quantile function checks inputs and has correct output", {
+#   alpha <- 2
+#   beta <- 5
+#   b <- BetaDistribution$new(alpha, beta)
+#   probs <- c(0.1, 0.2, 0.5)
+#   expect_silent(b$quantile(probs))
+#   probs <- c(0.1, NA, 0.5)
+#   expect_error(b$quantile(probs), class="probs_not_defined")
+#   probs <- c(0.1, "boo", 0.5)
+#   expect_error(b$quantile(probs), class="probs_not_numeric")
+#   probs <- c(0.1, 0.4, 1.5)
+#   expect_error(b$quantile(probs), class="probs_out_of_range")
+#   probs <- c(0.1, 0.2, 0.5)
+#   expect_length(b$quantile(probs),3)
+# })
+# 
+# test_that("Extreme mode values are defined", {
+#   alpha <- 1
+#   beta <- 1
+#   b <- BetaDistribution$new(alpha, beta)
+#   expect_equal(b$mode(), 0.5)
+#   alpha <- 0.5
+#   beta <- 0.5
+#   b <- BetaDistribution$new(alpha, beta)
+#   expect_true(is.na(b$mode()))
+#   alpha <- 0.5
+#   beta <- 1.5
+#   b <- BetaDistribution$new(alpha, beta)
+#   expect_equal(b$mode(), 0)
+#   alpha <- 1.5
+#   beta <- 0.5
+#   b <- BetaDistribution$new(alpha, beta)
+#   expect_equal(b$mode(), 1)
+# })
+# 
+# test_that("random sampling is from a Beta distribution", {
+#   alpha <- 2
+#   beta <- 5
+#   n <- 1000
+#   b <- BetaDistribution$new(alpha, beta)
+#   osamp <- sapply(1:n, FUN=function(i) {
+#     b$sample()
+#     rv <- b$r()
+#     return(rv)
+#   })
+#   expect_length(osamp, n)
+#   # 99.9% confidence limits; expected test failure rate is 0.1%, 
+#   # skip for CRAN
+#   skip_on_cran()
+#   esamp <- rbeta(n, shape1=alpha, shape2=beta)
+#   ht <- ks.test(osamp, esamp)
+#   expect_true(ht$p.value > 0.001)
+# })
